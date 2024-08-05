4d4a2636963e3816e7771f22ee1302bf8c3d2eca
n-hokari <hokari1204@gmail.com>
2024-08-05 23:44:51 +0900

    Initial commit

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -1,6 +1,8 @@
 require_relative "item_manager"
+require_relative "ownable"
 
 class Cart
+  include Ownable
   include ItemManager
 
   def initialize(owner)
@@ -28,12 +30,16 @@ class Cart
   #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
   #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
   #   - カートの中身（Cart#items）が空になること。
-
-  # ## ヒント
-  #   - カートのオーナーのウォレット ==> self.owner.wallet
-  #   - アイテムのオーナーのウォレット ==> item.owner.wallet
-  #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
-  #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
+   
+    @items.each do |item|
+      withdrawn_amount = owner.wallet.withdraw(item.price)
+      item.owner.wallet.deposit(withdrawn_amount)
+    
+      item.owner = owner
+  
+    end
+  
+    @items = []
   end
 
 end
